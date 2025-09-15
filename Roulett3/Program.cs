63a31b8ea7eebf2fb8c832389b1bd14096155c62
MHD-Hassan1 <14MA0801@amal.se>
2025-09-15 11:00:58 +0200

    Add project files.

---
+++ Roulett3/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Random rand = new Random();
+
+Console.WriteLine("Welcome to Roulette!!");
+
+Console.WriteLine("How much is ur budget"); 
+int budget = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("And how much do u want to bet today :)");
+int bet = Convert.ToInt32(Console.ReadLine());
+
+if  (budget < bet)
+{
+    Console.WriteLine(" U dont have so much money :(");
+}
