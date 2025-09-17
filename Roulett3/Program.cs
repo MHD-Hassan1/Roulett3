34e42ac3fe2ff685db22050632bb4476e4fb4c2a
MHD-Hassan1 <14MA0801@amal.se>
2025-09-17 11:23:11 +0200

    1

--- Roulett3/Program.cs
+++ Roulett3/Program.cs
@@ -1,15 +1,57 @@
 ﻿
 Random rand = new Random();
+Console.ForegroundColor = ConsoleColor.Red;
 
 Console.WriteLine("Welcome to Roulette!!");
 
+Console.ForegroundColor = ConsoleColor.White;
+
 Console.WriteLine("How much is ur budget"); 
 int budget = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
 
-Console.WriteLine("And how much do u want to bet today :)");
-int bet = Convert.ToInt32(Console.ReadLine());
-
-if  (budget < bet)
+while (budget > 0)
 {
-    Console.WriteLine(" U dont have so much money :(");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine($"Your current budget: {budget}" + "$");
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("How much do you want to bet");
+    int bet = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+
+    if (bet > budget)
+    {
+    
+        Console.WriteLine("Get the fuck out of here. Nigga boy");
+        
+    }
+
+    Console.WriteLine("Do you want to bet on red or black?");
+    string choice = Console.ReadLine();
+    int number = rand.Next(0, 37);
+    string color;
 }
+
+
+
+
+
+// Förenklad regel udda = röd, jämn = svart 0 = grönt
+
+
+
+
+if (number == 0)
+{
+    color = "green";
+} else if (number % 2 == 0 )
+{
+    color = "svart";
+}
+else
+{
+    color = "red";
+}
+
+
+
